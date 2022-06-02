b91bb85f4d9e0c015a1675dbe10607b58dbd39be
mustafa <mustafaegebozkay555a@gmail.com>
2022-06-02 14:09:26 +0300

    menu or gui files was edited

--- src/battery_control_pkg/scripts/Battery_GUI/PYQT5_GUI.py
+++ src/battery_control_pkg/scripts/Battery_GUI/PYQT5_GUI.py
@@ -7,28 +7,29 @@ from PyQt5.QtWidgets import QDialog
 from PyQt5.QtWidgets import QDialogButtonBox
 from PyQt5.QtWidgets import QFormLayout
 from PyQt5.QtWidgets import QLineEdit
-from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
 
 
-class Dialog(QDialog):
+class Dialog(QDialog): #
     """Dialog."""
 
     def __init__(self, parent=None):
         """Initializer."""
         super().__init__(parent)
-        self.setWindowTitle('QDialog')
-        dlgLayout = QVBoxLayout()
-        formLayout = QFormLayout()
-        formLayout.addRow('Name:', QLineEdit())
-        formLayout.addRow('Age:', QLineEdit())
+        self.setWindowTitle('QDialog') # set the title of the window to QDialog (the title of the dialog)
+        dlgLayout = QHBoxLayout() #create a horizontal layout for the dialog box to be displayed in the center of the screen
+
+        formLayout = QFormLayout() # create a form layout to hold the line edits
+        formLayout.addRow('Name:', QLineEdit()) # add a row to the form layout with the label 'Name:' and the line edit
+        formLayout.addRow('Age:', QLineEdit()) 
         formLayout.addRow('Job:', QLineEdit())
         formLayout.addRow('Hobbies:', QLineEdit())
-        dlgLayout.addLayout(formLayout)
-        btns = QDialogButtonBox()
+        dlgLayout.addLayout(formLayout) # add the form layout to the dialog layout
+        btns = QDialogButtonBox() # create a dialog button box to hold the buttons for the dialog box
         btns.setStandardButtons(
-            QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
-        dlgLayout.addWidget(btns)
-        self.setLayout(dlgLayout)
+            QDialogButtonBox.Cancel | QDialogButtonBox.Ok) # set the buttons to be cancel and ok
+        dlgLayout.addWidget(btns) # add the button box to the dialog layout
+        self.setLayout(dlgLayout) # set the layout of the dialog box to the dialog layout
 
 
 if __name__ == '__main__':
